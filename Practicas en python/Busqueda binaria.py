6fc09fbd349ad716c2e5852a1fa0a2b9259f7a10
CrisForchue <forchuel@gmail.com>
2024-07-29 22:57:43 -0400

    Practica 29/Jul (final)

--- Practicas en python/Busqueda binaria.py
+++ Practicas en python/Busqueda binaria.py
@@ -2,15 +2,12 @@ import random
 import time
 
 def busquedaIngenua(lista, objetivo): 
-    for i in range (len(lista)):
+    for i in range(len(lista)):
         if lista[i] == objetivo:
             return i 
     return -1
 
-list = [1, 2, 3, 4, 5, 6]
-print(busquedaIngenua(list, 5)) #Imprime el indice 
-
-def busquedaBinaria(lista, objetivo, lim_inferior=None,lim_superior=None):
+def busquedaBinaria(lista, objetivo, lim_inferior=None, lim_superior=None):
     if lim_inferior is None:
         lim_inferior = 0
     if lim_superior is None:
@@ -24,6 +21,36 @@ def busquedaBinaria(lista, objetivo, lim_inferior=None,lim_superior=None):
     if lista[punto_medio] == objetivo: 
         return punto_medio
     elif objetivo < lista[punto_medio]:
-        return busquedaBinaria(lista, objetivo, lim_inferior, punto_medio-1)
+        return busquedaBinaria(lista, objetivo, lim_inferior, punto_medio - 1)
     else: 
-        return busquedaBinaria(lista, objetivo, punto_medio+1,)
+        return busquedaBinaria(lista, objetivo, punto_medio + 1, lim_superior)
+    
+if __name__ == '__main__':
+    mi_lista = [1, 2, 3, 4, 5, 6]
+    print(busquedaIngenua(mi_lista, 5)) # Imprime el índice 
+    print(busquedaBinaria(mi_lista, 5))
+
+    # Crear lista ordenada con 1000 números aleatorios.
+    tamano = 20000
+    conjuntoIncial = set()
+
+    while len(conjuntoIncial) < tamano:
+        conjuntoIncial.add(random.randint(-3*tamano, 3*tamano)) # Selecciona números desde -3000 hasta 3000
+
+    lista = sorted(list(conjuntoIncial))
+
+    # Medir el tiempo de búsqueda ingenua
+    inicio = time.time()
+    for objetivo in lista: 
+        busquedaIngenua(lista, objetivo)
+    fin = time.time()
+    print(f"Tiempo de búsqueda ingenua: {fin - inicio} segundos")
+
+    # Medir el tiempo de búsqueda binaria
+    inicio = time.time()
+    for objetivo in lista: 
+        busquedaBinaria(lista, objetivo)
+    fin = time.time()
+    print(f"Tiempo de búsqueda binaria: {fin - inicio} segundos")
+
+
